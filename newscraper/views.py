f089cad8559492634c4483b8235cf986d14ad04d
KevinLCai <65425655+KevinLCai@users.noreply.github.com>
2020-09-18 09:35:29 +0100

    multiprocessing

--- newscraper/views.py
+++ newscraper/views.py
@@ -6,19 +6,81 @@ from django.http import JsonResponse
 from django.shortcuts import HttpResponse, HttpResponseRedirect, render
 from django.urls import reverse
 from django.views.decorators.csrf import csrf_exempt
+from .web_scraper import *
+import time
+import multiprocessing
 
-from .web_scraper import scrape
+import requests
+from bs4 import BeautifulSoup
+import webbrowser
+from collections import Counter
+from string import punctuation
+from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS as stop_words
+import concurrent.futures
 
+def finalise(url):
+    try:
+        array2 = []
+        article = requests.get(url)
+        article = article.content
+        article = BeautifulSoup(article, 'lxml')
+        paragraphs = article.find_all('p')
+
+        string_concat = ''
+        #Concatenate text into one string!
+        for paragraph in paragraphs:
+            string = paragraph.text
+            string_concat += string + ' '
+        text = string_concat
+
+        text = text.replace("Share this withEmailFacebookMessengerMessengerTwitterPinterestWhatsAppLinkedInCopy this linkThese are external links and will open in a new window", "")
+        text = text.replace("The BBC is not responsible for the content of external sites. Read about our approach to external linking.", "")
+        text = text.replace("Share this with Email Facebook Messenger Messenger Twitter Pinterest WhatsApp LinkedIn", "")
+        text = text.replace("Copy this link These are external links and will open in a new window", "")
+
+        #Tokenise
+        tokens = tokenizer(text)
+        sents = sent_tokenizer(text)
+        word_counts = count_words(tokens)
+        word_counts
+        freq_dist = word_freq_distribution(word_counts)
+        freq_dist
+        sent_scores = score_sentences(sents, freq_dist)
+        sent_scores
+
+        #Generate summary
+        summary, summary_sent_scores = summarize(sent_scores, 3)
+        #print(titles[number-1] + '\n')
+        #print(summary)
+        array2.append(summary)
+        
+        #print('\n')
+    except IndexError:
+        pass
+    return summary
 
 def index(request):
     return render(request, "newscraper/index.html")
 
 def return_page(request, catagory):
+    global final_summaries
+
     titles = []
-    summaries = []
-    info = scrape(catagory, titles, summaries)
-    titles = info[0]
-    summaries = info[1]
+    
+    info = scrape1(catagory, titles)
+
+    # #Multiprocessing to speed up scraping and summarising
+    # with concurrent.futures.ProcessPoolExecutor() as executor:
+    urls = info[0]
+    titles = info[1]
+    
+    pool = multiprocessing.Pool()
+    summaries = [pool.map(finalise, urls)]
+    pool.close()
+    pool.join()
+    summaries = summaries[0]
+
+    # if final_summaries:
     return render(request, "newscraper/catagory.html", {
         'catagory' : catagory.title() if catagory != 'us2020' else 'US Election',
         'title1' : titles[0],
